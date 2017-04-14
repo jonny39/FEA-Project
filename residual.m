15801c24719889b2eeaa841e02172abeb537e511
Jon Erickson <erickson.jonathan01@gmail.com>
2017-04-14 11:14:55 -0600

    Updated RUN_ME to include mesh building and IEN

--- residual.m
+++ residual.m
@@ -1,4 +1,4 @@
-function [R] = residual(LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en)
+function [R] = residual(LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_e)
 
 % Initializations
 r_e = zeros(n_en*n_dof+n_dof,1);
@@ -6,7 +6,6 @@ R = zeros(n_en*n_dof+n_dof,1);
 
 for e = 1:length(mesh)
 	r_e = r_e*0;
-	x_a = x_aFunc(mesh);
 	
 	% Assembly element r_e
 	D = buildD(E, nu); 	% Inputs are material properties
@@ -14,7 +13,7 @@ for e = 1:length(mesh)
 	
 	for igpt = 1:n_int
 		[N, dN_dxi, dN_deta] = lagrange2D(pts(igpt), eta(igpt), p, q);  % what are p and q 
-		[dN_dx, dN_dy, detJ, x] = lagrange2Dspatial(pts(igpt), eta(igpt), p, q, N, dN_dxi, dN_deta, x_e);
+		[dN_dx, dN_dy, detJ, x] = lagrange2Dspatial(pts(igpt), eta(igpt), p, q, N, dN_dxi, dN_deta, nodes_e);
 		
 		sigma = computeStress(d(e), dN_dx, dN_dy);
 		bf = getBodyForce(x);
