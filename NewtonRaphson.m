b8fdb6c7139d9dd31a1e4eb5e4c94d180d0d2a88
Tyler <tamathis16@gmail.com>
2017-04-17 11:08:50 -0600

    no message

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -1,25 +1,25 @@
-function d = NewtonRaphson(mesh, LM, )
+function d = NewtonRaphson(mesh, LM, E, nu, p, q, n_int, n_dof, n_en, nodes_el)
 	F_inc0 = 1;
-	conv = 1e-8;
+	epsilon = 1e-8;
 	i_max = 10;
 	n_max = 1./F_inc0;
 	n = 0;
-	d = I dont know
+	d = zeros((n_dof+1)*n_en, 1);
 	
 	while n < n_max
 		F_inc = Finc0*(n+1)/n_max;
-		R0 = residual(F_inc, LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_e);
+		R0 = residual(F_inc, LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_el);
 		R = R0;
 		i = 0;
 		
 		while i < i_max
-			K = stiffness(LM, mesh, d, E, nu, n_int, n_f, n_dof, n_en, nodes_e);
+			K = stiffness(LM, mesh, d, E, nu, n_int, p, n_dof, n_en, nodes_el);
 			delta = K/R
 			d = d + delta;
 			
-			R0 = residual(F_inc, LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_e);
+			R0 = residual(F_inc, LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_el);
 			
-			if norm(R) < norm(R0)
+			if norm(R) <= norm(R0)*epsilon
 				break;
 			end
 			
