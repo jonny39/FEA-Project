0bb0d9d183ead294ea806b74f61965449a0a2c33
Jon Erickson <erickson.jonathan01@gmail.com>
2017-04-17 11:52:53 -0600

    no message

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -2,7 +2,7 @@ function d = NewtonRaphson(mesh, LM, E, nu, p, q, n_int, n_dof, n_en, nodes_el)
 	F_inc0 = 1;
 	epsilon = 1e-8;
 	i_max = 10;
-	n_max = 1./F_inc0;
+	n_max = ceil(1./F_inc0);
 	n = 0;
 	d = zeros((n_dof+1)*n_en, 1);
 	
@@ -14,7 +14,7 @@ function d = NewtonRaphson(mesh, LM, E, nu, p, q, n_int, n_dof, n_en, nodes_el)
 		
 		while i < i_max
 			K = stiffness(LM, mesh, d, E, nu, n_int, p, n_dof, n_en, nodes_el);
-			delta = K/R
+			delta = K/R;
 			d = d + delta;
 			
 			R0 = residual(F_inc, LM, mesh, d, E, nu, n_int, p, q, n_dof, n_en, nodes_el);
@@ -23,10 +23,10 @@ function d = NewtonRaphson(mesh, LM, E, nu, p, q, n_int, n_dof, n_en, nodes_el)
 				break;
 			end
 			
-			i = i + 1
+			i = i + 1;
 		end
 		
-		n = n + 1
+		n = n + 1;
 			
 	end
 end
