fc57f83b9c3043f04aeb187a444f9049d1067180
calebbbb <calebb10@yahoo.com>
2017-04-21 13:31:10 -0600

    no message

--- RUN_ME.m
+++ RUN_ME.m
@@ -8,10 +8,10 @@ format compact
 epsilon = 1e-12;
 
 %problem number
-problemNumber =  2;
+problemNumber =  1;
 
 %displacement amount on right face
-displacement = [1 0]; %x y
+displacement = [0.01 0]; %x y
 h = [0.01 0]; %x y
 body_force = [0 0];
 
@@ -19,21 +19,19 @@ body_force = [0 0];
 elementType = 'rect'; %'rad' for radial, 'rect' for rectilinear
 
 %basis function order
-p = 1;
+p = 2;
 q = p;
 
 %material parameters
 E = 1;
-nu = .1;
+nu = 0.3;
 n_dof = 2;
 
 %define mesh geometry
 %for radial nodes, n is radial, m is circumfirential
 %for rectangular nodes, n is horizontal, m is vertical
-n = 4;
-m = 4;
-n = 20;
-m = 20;
+n = 5;
+m = 5;
 he_n = 1/n;
 he_m = 1/m;
 
