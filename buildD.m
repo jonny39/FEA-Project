e9d128fbabcb1e391ab701c81699f5fb52705fbc
Tyler <tamathis16@gmail.com>
2017-04-21 13:28:25 -0600

    no message

--- buildD.m
+++ buildD.m
@@ -2,7 +2,9 @@ function D = buildD(E, nu)
 	lambda = nu*E/((1+nu)*(1-2*nu));
 	mu = E/(2*(1-nu));
 	
-	D = [2*mu+lambda    lambda          0;
-		 lambda         2*mu+lambda     0;
-		 0              0               mu];
+	lambda_bar = 2*mu*lambda/(lambda + 2*mu);
+	
+	D = [2*mu+lambda_bar    lambda_bar          0;
+		 lambda_bar         2*mu+lambda_bar     0;
+		 0             		0               	mu];
 end
