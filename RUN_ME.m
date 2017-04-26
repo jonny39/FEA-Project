23e36e2cdc9836261783ab337e1a16f501422c7a
Jon Erickson <erickson.jonathan01@gmail.com>
2017-04-26 10:36:13 -0600

    no message

--- RUN_ME.m
+++ RUN_ME.m
@@ -12,7 +12,7 @@ problemNumber = 1;
 
 %displacement amount on right face
 displacement = [1 0]; %x y
-h = [.1 0]; %x y
+h = [0 0]; %x y
 body_force = [0 0];
 
 %element type
@@ -24,14 +24,14 @@ q = p;
 
 %material parameters
 E = 1;
-nu = 0.3;
+nu = .3;
 n_dof = 2;
 
 %define mesh geometry
 %for radial nodes, n is radial, m is circumfirential
 %for rectangular nodes, n is horizontal, m is vertical
-m = 3;
-n = 3;
+m = 10;
+n = 10;
 
 he_n = 1/n;
 he_m = 1/m;
@@ -51,7 +51,7 @@ end
 %what value needs to be plotted. 1 = x displacement, 2 = y displacement, 
 %3 = von mises stress, 4 = sigma x, 5 = sigma y, 6 = torsional stress
 %7 = node locations
-plot_desired = 7;
+plot_desired = 6;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -74,7 +74,9 @@ dSolution = NewtonRaphson(mesh, LM, IEN, ID, E, nu,...
 
 %plot solution
 dSolution
-plotter(dSolution,mesh,plot_desired,m,n,n_dof,LM_plot,E,nu,n_en,p,q,nodes_e,IEN)
+d0 = dSolution*0;
+plotter(dSolution,mesh,plot_desired,...
+        m,n,n_dof,LM_plot,E,nu,n_en,p,q,nodes_e,IEN)
 
 if isnan(sum(dSolution)) == 0
     load handel.mat
